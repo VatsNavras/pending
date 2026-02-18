f354a2d9f5733bf8d904884308359a48ac34e510
VatsNavras <vatsankur1730@gmail.com>
2026-02-18 16:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 from sheets import load_sheet, update_status, ensure_columns
 
-print("Sheets file loaded")
 
 
 st.set_page_config(page_title="Order Status App", layout="wide")
